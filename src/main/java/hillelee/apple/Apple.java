95a678ff595cc0edc891db729c7ed5a2ec3b9ac0
Vladyslove <od.vladyslav@gmail.com>
2017-11-16 21:24:11 +0200

    most part of lesson2 are finished. Features of Stream are learned

--- src/main/java/hillelee/apple/Apple.java
+++ src/main/java/hillelee/apple/Apple.java
@@ -1,9 +1,12 @@
 package hillelee.apple;
 
+import com.google.common.collect.ImmutableList;
 import hillelee.defaultMethod.Fruit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -13,5 +16,10 @@ public class Apple implements Fruit {
 
     private String color;
     private Integer weight;
+    private List<String> worms = ImmutableList.of("worm1", "worm2", "worm3");
 
+    public Apple(String color, Integer weight) {
+        this.color = color;
+        this.weight = weight;
+    }
 }
--- src/test/java/hillelee/apple/AppleSelectorTest.java
+++ src/test/java/hillelee/apple/AppleSelectorTest.java
@@ -6,12 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class AppleSelectorTest {
     private List<Apple> apples;
@@ -20,10 +21,10 @@ public class AppleSelectorTest {
     public void setUp() {
         apples = ImmutableList.of(new Apple("RED", 100),
                 new Apple("RED", 120),
-                new Apple("GREEN", 110),
+                new Apple("RED", 110),
                 new Apple("GREEN", 130),
-                new Apple("RED", 150),
-                new Apple("RED", 100));
+                new Apple("GREEN", 101),
+                new Apple("RED", 150));
     }
 
     @Test
@@ -102,11 +103,31 @@ public class AppleSelectorTest {
         // with Lambda
         List<Apple> filtered = AppleSelector.filter(apples, apple -> apple.getWeight() > 120);
 */
+
+        Predicate<Apple> heavierThan120 = apple -> apple.getWeight() > 120;
         List<Apple> filtered = apples.stream()
-                .filter(apple -> apple.getWeight() > 120)
+                .filter(heavierThan120 /*or apple -> apple.getWeight() > 120*/)
                 .collect(Collectors.toList());
 
         assertThat(filtered, hasSize(2));
+    }
+
+    @Test
+    public void filterByAnonymousPredicatesCompositionWithStreams () throws Exception {
+
+        Predicate<Apple> heavierThan120 = apple -> apple.getWeight() > 120;
+        Predicate<Apple> isRed = apple -> apple.getColor().equals("RED");
+        Predicate<Apple> HeaavierThan120AndRed = isRed.and(heavierThan120);
+        /* the same Without variables - too UGLYYYYYYYYYY:
+        HeaavierThan120AndRed = ((Predicate<Apple>) (apple -> apple.getWeight() > 120)).
+                and((apple -> apple.getColor().equals("RED")));*/
+//        Predicate<Apple> HeaavierThan120OrRed = isRed.or(heavierThan120);
+//        Predicate<Apple> HeaavierThan120ExceptRed = (isRed.negate()).and(heavierThan120);
+        List<Apple> filtered = apples.stream()
+                .filter(HeaavierThan120AndRed)
+                .collect(Collectors.toList());
+
+        assertThat(filtered, hasSize(1));
     }
 /*//    added before <T> list
     @Test
@@ -124,6 +145,99 @@ public class AppleSelectorTest {
 
     @Test
     public void mapToColor() throws Exception {
+        List<String> colors = apples.stream()
+//                .map(apple -> apple.getColor())
+                //again the same 2 lines - IIdea give a hint to replace lambda with method reference
+                // не до конца понял объяснение по Function/Consumer для себя запомнил, что
+                // 2 apple сокращаются до одного Apple и ставится :: как отображение 2ух этих объектов
+                .map(Apple::getColor)
+                .collect(Collectors.toList());
 
+        assertThat(colors, hasSize(6));
+        assertSame(colors.get(0), "RED");
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+
+    @Test
+    public void printApples() throws Exception {
+
+        // the same as with Streams in next line
+        for (Apple apple : apples) System.out.print(apple);
+
+        apples.stream().forEach(apple -> System.out.print(apple));
+        // 2 Consumers - that's why we can replace
+        //the same 2 lines - IIdea give a hint to replace lambda with method reference
+        apples.stream().forEach(System.out::print);
+    }
+
+    @Test
+    public void adjustColor() throws Exception {
+        ColorAdjuster colorAdjuster = new ColorAdjuster();
+
+        /*      Function<String, String> adjust = colorAdjuster::adjust;
+
+        BiFunction<ColorAdjuster, String, String > adjustWithAdjuster = ColorAdjuster::adjust;*/
+
+        apples.stream()
+                .map(Apple::getColor)
+                .map(colorAdjuster::adjust)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void executionFlow() throws Exception {
+        /*List<String> collect = */apples.stream()
+                .filter(apple -> apple.getWeight() > 113)
+                .map(Apple::getColor)
+                .peek(System.out::println)
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void findFirst() throws Exception {
+        apples.stream()
+                .filter(apple -> apple.getWeight() > 159)
+                .findFirst()
+                .map(Apple::getColor)
+                .ifPresent(System.out::println);
+    }
+
+    @Test
+    public void intStream() throws Exception {
+        IntSummaryStatistics sum = apples.stream()
+//                .mapToInt(Apple::getWeight)
+                // the same 1 line above and 2 below
+                .map(Apple::getWeight)
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
+        System.out.println(sum);
+    }
+
+    @Test
+    public void hashMapWhereKeyIsWeightAndValueIsAppleTest() throws Exception {
+        Map<Integer, Apple> weightToApple = apples.stream()
+                .collect(Collectors.toMap(Apple::getWeight, Function.identity()));
+
+                assertThat(weightToApple.get(100), is(apples.get(0)));
+    }
+
+    @Test
+    public void hashMapWhereKeyIsColorAndValueIsListOfWeights() throws Exception {
+        Map<String, List<Apple>> collect = apples.stream().
+                collect(Collectors.groupingBy(Apple::getColor, Collectors.toList()));
+        System.out.println(collect.get("RED"));
+    }
+
+    @Test
+    public void getAllWorms() throws Exception {
+        apples.stream()
+                .flatMap(apple -> apple.getWorms().stream())
+                /*.map(Apple::getWorms)
+                .flatMap(List::stream)*/
+                .forEach(System.out::print);
     }
 }
