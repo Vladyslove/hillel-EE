dae39baf36f9ba2f5b386ed92d80046392939008
Vladyslove <od.vladyslav@gmail.com>
2017-11-23 19:48:48 +0200

    hw3. Part 4 is done. ProblemSolver class with Streams. Not easy for understanding

--- src/main/java/hillelee/reflection/ProblemSolver.java
+++ src/main/java/hillelee/reflection/ProblemSolver.java
@@ -4,9 +4,14 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 
 public class ProblemSolver {
+
+ /*   //without Stream
     public String solve(Object problem) {
         Class<?> aClass = problem.getClass();
         Method[] methods = aClass.getMethods();
@@ -20,9 +25,41 @@ public class ProblemSolver {
             }
         }
         throw new RuntimeException("There is no CorrectAnswer");
+    }*/
+   /* // WithStreamsButWithoutflatMap
+    @SneakyThrows
+    public String solve(Object problem) {
+        Class<?> aClass = problem.getClass();
+         return Arrays.stream(aClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(CorrectAnswer.class))
+                .map(method -> (String) method.invoke(problem))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer Annotation"));
+    }*/
+
+    //WithStreamsAndflatMap
+//    @SneakyThrows
+    public String solve(Object problem) {
+        return   Stream.of(problem)
+                .map(Object::getClass)
+                .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
+                .filter(method -> method.isAnnotationPresent(CorrectAnswer.class))
+                .map(invokeOn(problem))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer Annotation"));
     }
 
-    //    @SneakyThrows(RuntimeException.class) // won't work
+    private Function<Method, String > invokeOn(Object object) {
+        return method -> {
+            try {
+                return (String) method.invoke(object);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+//    @SneakyThrows(RuntimeException.class) // won't work
 //    @SneakyThrows({IllegalAccessException.class, InvocationTargetException.class})
 //    @SneakyThrows(ReflectiveOperationException.class) // first parent of two upper classes
     @SneakyThrows
@@ -30,7 +67,7 @@ public class ProblemSolver {
         Class<?> aClass = problem.getClass();
         Method[] methods = aClass.getMethods();
         for (Method method : methods) {
-            if (method.isAnnotationPresent(CorrectAnswer.class)) {
+            if (method.isAnnotationPresent( CorrectAnswer.class)) {
                 return (String) method.invoke(problem);
             }
         }
