2c0a1b1c329655401273a420e4e675466387de12
Vladyslove <od.vladyslav@gmail.com>
2017-11-24 02:32:03 +0200

    Lesson4. Spring boot. First app was successfully build!

---
+++ src/main/java/hillelee/pet/PetController.java
@@ -0,0 +1,19 @@
+package hillelee.pet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetController {
+
+//    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    @GetMapping(value = "/greeting")
+    public String helloWorld() {
+        return "helloWorld";
+    }
+
+    @GetMapping(value = "/greeting2")
+    public String helloWorld2() {
+        return "helloWorld2";
+    }
+}
