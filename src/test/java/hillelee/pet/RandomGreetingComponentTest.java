c629a0c812a33208023bdab282c86b074d81b74f
Vladyslove <od.vladyslav@gmail.com>
2017-11-26 07:42:49 +0200

    HW4. part5 is done. Not best decision, but suitable and understandable for me

--- src/test/java/hillelee/pet/RandomGreetingComponentTest.java
+++ src/test/java/hillelee/pet/RandomGreetingComponentTest.java
@@ -4,9 +4,7 @@ import hillelee.RandomGreetingComponent;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class RandomGreetingComponentTest {
 
@@ -14,8 +12,9 @@ public class RandomGreetingComponentTest {
     public void displayRandomGreetingComponent() throws Exception {
         RandomGreetingComponent randomGreetingComponent = new RandomGreetingComponent();
         Map<String, Integer> greetingsMap = new HashMap<>();
-        int sizeOfSelection = 100;
+        int sizeOfSelection = 1_000;
         String greeting;
+
         for (int i = 0; i < sizeOfSelection; i++) {
             greeting = randomGreetingComponent.displayRandomGreetingComponent();
             if (greetingsMap.containsKey(greeting)) {
@@ -24,25 +23,54 @@ public class RandomGreetingComponentTest {
                 greetingsMap.put(greeting, 1);
             }
         }
+
             // without if else - it's hard to understand at once
 //            greetingsMap.put(greeting, greetingsMap.containsKey(greeting)? greetingsMap.get(greeting) + 1 : 1);
-            Set<Map.Entry<String, Integer>> entrySet = greetingsMap.entrySet();
+//            Set<Map.Entry<String, Integer>> entrySet = greetingsMap.entrySet();
 //        double percent = computePercentage(e.getValue(), sizeOfSelection);
+//        entrySet.stream()
+//                .peek(e -> {
+//                    System.out.println(computePercentage(e.getValue(), sizeOfSelection));
+//                    Assert.assertTrue(computePercentage(e.getValue(), sizeOfSelection) <=10);
+//                });
+//        greetingsMap.entrySet();
+        printMap(greetingsMap);
 
-        entrySet.stream()
-                .peek(e -> {
-                    System.out.println(computePercentage(e.getValue(), sizeOfSelection));
-                    Assert.assertTrue(computePercentage(e.getValue(), sizeOfSelection) <=10);
-                });
-        greetingsMap.entrySet();
+        List<String> phrases = new ArrayList<>();
+        phrases.add("bonjour world with Class and method and Component");
+        phrases.add("hello world with Class and method and Component");
+        phrases.add("hola world with Class and method and Component");
 
+        double percent1 = (double) (greetingsMap.get(phrases.get(0))) /sizeOfSelection * 100;
+        double percent2 = (double) (greetingsMap.get(phrases.get(1))) /sizeOfSelection * 100;
+        double percent3 = (double) (greetingsMap.get(phrases.get(2))) / sizeOfSelection * 100;
+
+        double deviation1 = Math.abs(1 - (percent1/(100/3))) * 100;
+        double deviation2 = Math.abs(1 - (percent2/(100/3))) * 100;
+        double deviation3 = Math.abs(1 - (percent3/(100/3))) * 100;
+
+        double averageDeviation = (deviation1 + deviation2 + deviation3) / 3;
+        /*System.out.println("percent of 'bonjour world with Class and method and Component' is: " + percent1);
+        System.out.println("deviation percent 1 is : " + deviation1);
+        System.out.println("percent of 'hello world with Class and method and Component' is: " + percent2);
+        System.out.println("deviation percent 2 is : " + deviation2);
+        System.out.println("percent of 'hola world with Class and method and Component' is: " + percent3);
+        System.out.println("deviation percent 3 is : " + deviation3);*/
+        System.out.println("average deviation is: " + averageDeviation);
+        Assert.assertTrue(averageDeviation < 10);
     }
 
-    private double computePercentage(Integer actual, Integer sizeOfSelection) {
+    private static void printMap(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            String key = pair.getKey();
+            Integer value = pair.getValue();
+            System.out.println("key is: " + key + ". value is: " + value);
+        }
+    }
+
+    /*private double computePercentage(Integer actual, Integer sizeOfSelection) {
         double expected = sizeOfSelection / 3;
-        return (Math.abs(actual - expected) * 100.0) / expected;
-    }
 
-
-
+        return (Math.abs(actual - expected)) / expected * 100;
+    }*/
 }
