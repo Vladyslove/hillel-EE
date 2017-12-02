ae266074bfaf3928337da9a87fe3c38080200c81
Vladyslove <od.vladyslav@gmail.com>
2017-12-02 04:28:12 +0200

    HW5. All methods are completed. Multithreading is only left

--- src/main/java/hillelee/doctor/DoctorController.java
+++ src/main/java/hillelee/doctor/DoctorController.java
@@ -7,11 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -26,9 +24,8 @@ public class DoctorController {
 
     private Integer counter = doctors.size();
 
-    //POST
     @PostMapping("/doctors")
-    public ResponseEntity<? super Doctor> createDoctor(@RequestBody Doctor doctor) {
+    public ResponseEntity<? /*super Doctor*/> createDoctor(@RequestBody Doctor doctor) {
 
         if (doctors.containsKey(doctor.getId())) {
             return ResponseEntity.badRequest().build();
@@ -37,66 +34,49 @@ public class DoctorController {
         return ResponseEntity.created(URI.create("doctors/" + counter)).build();
     }
 
-    // GET1
-//    @GetMapping("/doctors")
-    public Map<Integer, Doctor> getDoctor() {
-        return doctors;
-    }
-
-    // GET2
     @GetMapping("/doctors/{id}")
     public ResponseEntity<? super Doctor> getDoctorById(@PathVariable Integer id) {
-        if (id >= doctors.size()) {
+        if (!doctors.containsKey(id)) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(doctors.get(id));
     }
 
-    //GET3
-//    @GetMapping("/doctors")
-    public List<Doctor> getDoctors(@RequestParam Optional<String> specialisation) {
+    @GetMapping("/doctors")
+    public List<Doctor> getDoctors(@RequestParam Optional<String> specialisation,
+                                   @RequestParam Optional<String> name) {
 
         Predicate<Doctor> specialisationFilter = specialisation.map(this::filterBySpecialisation)
                 .orElse(doctor -> true);
+        Predicate<Doctor> nameFilter = name.map(this::filterByName).
+                orElse(doctor -> true);
+        Predicate<Doctor> complexFilter = specialisationFilter.and(nameFilter);
 
         return doctors.values().stream()
-                .filter(specialisationFilter)
+                .filter(complexFilter)
                 .collect(Collectors.toList());
     }
-        private Predicate<Doctor> filterBySpecialisation (String specialisation){
-            return doctor -> doctor.getSpecialisation().equals(specialisation);
-        }
-
-    //GET4 (not finished)
-    @GetMapping("/doctors")
-    public List<Doctor> getDoctorsByName(@RequestParam Optional<String> name) {
-
-        final Predicate<Doctor> nameFilter = name.map(this::filterByName).
-                orElse(doctor -> true);
-
-        return doctors.values().stream()
-                .filter(nameFilter)
-                .collect(Collectors.toList());
+    private Predicate<Doctor> filterBySpecialisation (String specialisation){
+        return doctor -> doctor.getSpecialisation().equals(specialisation);
     }
     private Predicate<Doctor> filterByName(String name) {
-            return doctor -> doctor.getName().equals(name);
+
+        return doctor -> doctor.getName().equals(name);
     }
 
-    //PUT1-3
     @PutMapping("/doctors/{id}")
-    public ResponseEntity<Doctor> updateDoctor (@PathVariable Integer id,
+    public ResponseEntity<? super Doctor> updateDoctor (@PathVariable Integer id,
                                                 @RequestBody Doctor doctor){
-        if (doctors.containsKey(doctor.getId())) {
-           return ResponseEntity.badRequest().build();
+        if (!doctors.containsKey(id)) {
+            return ResponseEntity.notFound().build();
         }
-        if (id < doctors.size()) {
+        if (!Objects.equals(doctor.getId(), doctors.get(id).getId())){
+            return ResponseEntity.badRequest().body("ID changing is forbidden!");
+        }
             doctors.put(id,doctor);
             return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
     }
 
-    //Delete
     @DeleteMapping("/doctors/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteDoctors(@PathVariable Integer id) {
