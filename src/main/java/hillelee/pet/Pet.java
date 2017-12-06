a606f84d7d74ae31623d2b231ca82bec4d2fae97
Vladyslav <od.vladyslav@gmail.com>
2017-12-06 02:01:06 +0200

    Lesson6. refactoring to 3 layer architecture. injecting configuration

--- src/main/java/hillelee/pet/Pet.java
+++ src/main/java/hillelee/pet/Pet.java
@@ -1,5 +1,7 @@
 package hillelee.pet;
 
+import java.util.Optional;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-class Pet {
+public class Pet {
+    private Integer id;
     private String name;
-    private String species;
+    private String specie;
     private Integer age;
+
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
 }
