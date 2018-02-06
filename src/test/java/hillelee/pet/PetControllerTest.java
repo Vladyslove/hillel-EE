9de93031f3197ffcf593c4cdb9aebb8c33d50f17
Vladyslav <od.vladyslav@gmail.com>
2018-02-06 08:41:16 +0200

    Lesson 11. Tests for PetController are added

--- src/test/java/hillelee/pet/PetControllerTest.java
+++ src/test/java/hillelee/pet/PetControllerTest.java
@@ -1,19 +1,32 @@
-package hillelee.pet;
+ package hillelee.pet;
 
+import com.google.common.io.Resources;
+import hillelee.store.Medicine;
+import hillelee.store.MedicineRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,13 +36,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PetControllerTest {
     @Autowired
     JpaPetRepository petRepository; // injection of Pets for future taking
-
+    @Autowired
+    MedicineRepository medicineRepository;
     @Autowired
     MockMvc mockMvc;
 
     @After
     public void cleanUp() {
         petRepository.deleteAll();
+        medicineRepository.deleteAll();
     }
 
     @Test
@@ -53,6 +68,100 @@ public class PetControllerTest {
                 .andExpect(jsonPath("$.content", hasSize(2)))
                 .andExpect(jsonPath("$.content[0].age", is(1)))
                 .andExpect(jsonPath("$.content[1].age", is(3)));
+    }
+
+    @Test
+    public void getPetByIdTest() throws Exception {  // for @GetMapping("/pets/{id}")
+        Integer id= petRepository.save(new Pet("Tom", "Cat", 3, LocalDate.now(), null, null)).
+                                    getId();
+
+        mockMvc.perform(get("/pets/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(id))) // вызывается не на Джававском объекте, а на JSONе, который на вернул Controller
+                .andExpect(jsonPath("$.name", is("Tom")))
+                .andExpect(jsonPath("$.*", hasSize(7)));
+
+    }
+
+    @Test
+    public void createPetTest() throws Exception { // for @PostMapping("/pets")
+//        String body = "{\"name\":\"Tom"\, \"specie\":\"Cat\"}"
+
+        String resourceName = "cat.json";
+        String body = readFile(resourceName);
+
+        mockMvc.perform(post("/pets")
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", containsString("/pets/")));
+
+        List<Pet> all = petRepository.findAll();
+        assertThat(all, hasSize(1));
+        assertThat(all.get(0).getName().get(), is("Tom"));
+    }
+
+    @Test
+    public void updatePetTest() throws Exception {
+
+        Integer id = petRepository.save(new Pet("Jerry", "Mouse", 1, LocalDate.now(), null, null))
+                .getId();
+
+        String body = readFile("cat.json");
+
+        mockMvc.perform(put("/pets/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk());
+
+        Pet pet = petRepository.findById(id).get();
+
+        assertThat(pet.getAge(), is(3));
+        assertThat(pet.getName().get(), is("Tom"));
+        assertThat(pet.getSpecie(), is("Cat"));
+//        assertThat(all.get(0).getName().get(), is("Tom"));
+
+    }
+
+    @Test
+    public void deletePetTest() throws Exception { // for @DeleteMapping("/pets/{id}")
+        Integer id= petRepository.save(new Pet("Tom", "Cat", 3, LocalDate.now(), null, null)).
+                getId();
+
+        mockMvc.perform(delete("/pets/{id}", id))
+                .andExpect(status().isNoContent());
+
+        Optional<Pet> maybePet = petRepository.findById(id);
+        assertFalse(maybePet.isPresent());
+
+    }
+    private String readFile(String resourceName) throws IOException {
+        return Resources.toString(Resources.getResource(resourceName), Charset.defaultCharset());
+    }
+
+    @Test
+    public void prescribeMedicineTest() throws Exception { //@PostMapping("/pets/{id}/prescription")
+        Integer id= petRepository.save(new Pet("Tom", "Cat", 3, LocalDate.now(), null, null)).
+                getId();
+
+        medicineRepository.save(new Medicine("Brilliantum greenus",1));
+
+        String body = readFile("prescription.json");
+
+        mockMvc.perform(post("pets/{id}/prescriptions", id)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(body))
+                .andExpect(status().isOk());
+
+        List<Prescription> prescriptions = petRepository.findById(id).get().getPrescriptions();
+
+        assertThat(prescriptions, hasSize(1));
+
+        Medicine greenus = medicineRepository.findByName("Brilliantum greenus").get();
+
+        assertThat(greenus.getQuantity(), is(0));
+
+
     }
 
 }
